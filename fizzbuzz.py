cdbf8eb59899a9bf6e484eb2cb98dc7f126a62ce
“yoon308” <yoon970308@gmail.com>
2022-08-17 22:39:06 +0900

    feat: Add feature that print 'fizzbuzz' condition: if  i is times of 15.
    I created fizzbuzz code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,9 @@
 for i in range(1,16+1):
-    if i%3==0:
-	print('fizz')
+    if i%15==0:
+        print('fizzbuzz')   
+    elif i%3==0:
+        print('fizz')
     elif i%5==0:
-	print('buzz')
+        print('buzz')
     else:
         print(i)
